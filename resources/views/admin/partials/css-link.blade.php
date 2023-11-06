e834220c8a4000a582384944d5def24c610d3d6d
mh akash <mhakash5000@gmail.com>
2023-11-06 10:41:23 +0600

    added dashboard

---
+++ resources/views/admin/partials/css-link.blade.php
@@ -0,0 +1,24 @@
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+<meta http-equiv="X-UA-Compatible" content="ie=edge" />
+<title>Admin Dashboard</title>
+
+<!-- font awesome -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<!-- CSS files -->
+<link href="{{ asset('dist') }}/css/tabler.min.css?1684106062" rel="stylesheet" />
+<link href="{{ asset('dist') }}/css/tabler-flags.min.css?1684106062" rel="stylesheet" />
+<link href="{{ asset('dist') }}/css/tabler-payments.min.css?1684106062" rel="stylesheet" />
+<link href="{{ asset('dist') }}/css/tabler-vendors.min.css?1684106062" rel="stylesheet" />
+<link href="{{ asset('dist') }}/css/demo.min.css?1684106062" rel="stylesheet" />
+<style>
+    @import url('https://rsms.me/inter/inter.css');
+
+    :root {
+        --tblr-font-sans-serif: 'Inter Var', -apple-system, BlinkMacSystemFont, San Francisco, Segoe UI, Roboto, Helvetica Neue, sans-serif;
+    }
+
+    body {
+        font-feature-settings: "cv03", "cv04", "cv11";
+    }
+</style>
